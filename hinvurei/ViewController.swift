122b299bb57315985367b4eb8b1fadb84fe4c6bb
三城勝美 <sanjo@sanjoukatsumi-no-macbook-air.local>
2015-11-08 09:43:13 +0900

    Initial Commit

---
+++ hinvurei/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hinvurei
+//
+//  Created by 三城勝美 on 2015/11/08.
+//  Copyright © 2015年 sei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
